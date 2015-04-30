0089d0fc658aee4c758cf282e40e7acca38b659f
kangkyu <kangkyu1111@gmail.com>
2015-04-30 13:07:40 -0700

    add index of burgers, out of the yml file

---
+++ app/controllers/burgers_controller.rb
@@ -0,0 +1,6 @@
+class BurgersController < ApplicationController
+  def index
+    yml_file_path = Rails.root.join("lib/tasks/list-of-hamburger-restaurants/list-of-hamburgers.yml")
+    @burgers = YAML.load_file(yml_file_path)
+  end
+end
